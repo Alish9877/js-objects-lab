20e82f2e33b89ea1ff276deda3d475c48f072605
alish9877 <alshaikhali302@gmail.com>
2024-11-06 23:36:59 +0300

    unti6 done

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const game = {
     { location: 'Saffron City', completed: false, difficulty: 6 },
     { location: 'Cinnabar Island', completed: false, difficulty: 7 },
     { location: 'Viridian City', completed: false, difficulty: 8 }
-  ],f
+  ],
   items: [
     { name: 'potion', quantity: 4 },
     { name: 'pokeball', quantity: 8 },
@@ -50,5 +50,38 @@ Exercise 5
 Solve Exercise 5 here:
 */
 game.party.push = (pokemon[1], pokemon[5], pokemon[20])
+
+/*
+Exercise 6
+1. Set the `completed` property to true for gyms with a difficulty below 3.
+2. Think about how you'd loop through the `gyms` array to check and update the `completed` property.
+
+
+Solve Exercise 6 here:
+*/
+// game.gyms[]
+for (let i = 0; i < game.gyms.length; i++) {
+  if (game.gyms[i].difficulty < 3)
+    game.gyms[i].completed = true
+}
+
+/*
+Exercise 7
+1. Evolve the starter Pokémon you added to your party earlier. Each starter Pokémon evolves into a specific one.
+2. How would you replace the current starter Pokémon in your party with its evolved form?
+
+Hint: 
+  - Pokemon 1: Bulbasaur evolves into Pokemon 2: Ivysaur
+  - Pokemon 4: Charmander evolves into Pokemon 5: Charmeleon
+  - Pokemon 7: Squirtle evolves into Pokemon 8: Wartortle
+  - Pokemon 25: Pikachu evolves into Pokemon 26: Raichu
+
+More Hints: The existing starter Pokemon will be *replaced* in your party with the Pokemon it evolved into. When working with an array of objects, the splice() array method is ideal for replacing one element with another. 
+
+
+Solve Exercise 7 here:
+*/
+
+
 
 console.log(game)
